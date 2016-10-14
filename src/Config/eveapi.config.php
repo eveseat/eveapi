27bc174077d25490309cc5e28769138747c5f441
Leon Jacobs <leonja511@gmail.com>
2016-10-14 12:46:09 +0200

    Version Bump

--- src/Config/eveapi.config.php
+++ src/Config/eveapi.config.php
@@ -21,7 +21,7 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 
 return [
 
-    'version'          => '1.0.17',
+    'version'          => '1.0.18',
 
     // PhealNG Specific Configuration
     'pheal'            => [
