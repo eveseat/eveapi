cd7b4f864487f6fa7ebdcb4c5ce480c1fec9f1b8
Leon Jacobs <leonja511@gmail.com>
2016-11-26 09:08:57 +0200

    Drop minimum stability, add prefer-stable and bump to 2.0.0-alpha2.

--- src/Config/eveapi.config.php
+++ src/Config/eveapi.config.php
@@ -21,7 +21,7 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 
 return [
 
-    'version'          => '2.0.0-alpha1',
+    'version'          => '2.0.0-alpha2',
 
     // PhealNG Specific Configuration
     'pheal'            => [
