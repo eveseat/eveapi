13811361f88c618d9b621dbe852c542b2d6f23ba
Leon Jacobs <leonja511@gmail.com>
2018-01-04 22:27:03 +0200

    Add character standings updater.

---
+++ src/Jobs/Character/Standings.php
@@ -0,0 +1,74 @@
+<?php
+
+/*
+ * This file is part of SeAT
+ *
+ * Copyright (C) 2015, 2016, 2017, 2018  Leon Jacobs
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+namespace Seat\Eveapi\Jobs\Character;
+
+
+use Seat\Eveapi\Jobs\EsiBase;
+use Seat\Eveapi\Models\Character\CharacterStanding;
+
+/**
+ * Class Standings
+ * @package Seat\Eveapi\Jobs\Character
+ */
+class Standings extends EsiBase
+{
+    /**
+     * @var string
+     */
+    protected $method = 'get';
+
+    /**
+     * @var string
+     */
+    protected $endpoint = '/characters/{character_id}/standings/';
+
+    /**
+     * @var int
+     */
+    protected $version = 'v1';
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     * @throws \Exception
+     */
+    public function handle()
+    {
+
+        $standings = $this->retrieve([
+            'character_id' => $this->getCharacterId(),
+        ]);
+
+        collect($standings)->each(function ($standing) {
+
+            CharacterStanding::firstOrNew([
+                'character_id' => $this->getCharacterId(),
+                'from_id'      => $standing->from_id,
+            ])->fill([
+                'from_type' => $standing->from_type,
+                'standing'  => $standing->standing,
+            ])->save();
+        });
+    }
+}
---
+++ src/database/migrations/2018_01_04_201942_create_character_standings_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCharacterStandingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+
+        Schema::create('character_standings', function (Blueprint $table) {
+
+            $table->increments('id');
+
+            $table->integer('character_id');
+            $table->integer('from_id');
+            $table->enum('from_type', ['agent', 'npc_corp', 'faction']);
+            $table->float('standing');
+
+            $table->index('character_id');
+            $table->index('from_id');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+
+        Schema::dropIfExists('character_standings');
+    }
+}
