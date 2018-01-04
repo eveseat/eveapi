d371385b35775d6a7edb17580adeae31d60ace0f
Leon Jacobs <leonja511@gmail.com>
2018-01-04 21:47:26 +0200

    Add character roles updater.

---
+++ src/Jobs/Character/Roles.php
@@ -0,0 +1,82 @@
+<?php
+
+/*
+ * This file is part of SeAT
+ *
+ * Copyright (C) 2015, 2016, 2017, 2018  Leon Jacobs
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+namespace Seat\Eveapi\Jobs\Character;
+
+
+use Seat\Eveapi\Jobs\EsiBase;
+use Seat\Eveapi\Models\Character\CharacterRole;
+
+/**
+ * Class Roles
+ * @package Seat\Eveapi\Jobs\Character
+ */
+class Roles extends EsiBase
+{
+    /**
+     * @var string
+     */
+    protected $method = 'get';
+
+    /**
+     * @var string
+     */
+    protected $endpoint = '/characters/{character_id}/roles/';
+
+    /**
+     * @var int
+     */
+    protected $version = 'v2';
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     * @throws \Exception
+     */
+    public function handle()
+    {
+
+        $roles = $this->retrieve([
+            'character_id' => $this->getCharacterId(),
+        ]);
+
+        foreach (['roles', 'roles_at_hq', 'roles_at_base', 'roles_at_other'] as $scope) {
+
+            // Add new roles for this scope
+            collect($roles->$scope)->each(function ($role) use ($scope) {
+
+                CharacterRole::firstOrCreate([
+                    'character_id' => $this->getCharacterId(),
+                    'role'         => $role,
+                    'scope'        => $scope,
+                ]);
+            });
+
+            // Remove roles that may no longer exist for this scope
+            CharacterRole::where('character_id', $this->getCharacterId())
+                ->whereScope($scope)
+                ->whereNotIn('role', $roles->$scope)
+                ->delete();
+        }
+    }
+}
---
+++ src/database/migrations/2018_01_04_165227_create_character_roles_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCharacterRolesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+
+        Schema::create('character_roles', function (Blueprint $table) {
+
+            $table->integer('character_id');
+            $table->string('role');
+            $table->enum('scope',
+                ['roles', 'roles_at_hq', 'roles_at_base', 'roles_at_other']);
+
+            $table->index('character_id');
+            $table->index('role');
+
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+
+        Schema::dropIfExists('character_roles');
+    }
+}
