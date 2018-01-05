f09a7757b055db083a5d53db4867286ddd902d23
Leon Jacobs <leonja511@gmail.com>
2018-01-05 09:14:37 +0200

    Add character titles updater.

---
+++ src/Jobs/Character/Titles.php
@@ -0,0 +1,77 @@
+<?php
+
+/*
+ * This file is part of SeAT
+ *
+ * Copyright (C) 2015, 2016, 2017, 2018  Leon Jacobs
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+namespace Seat\Eveapi\Jobs\Character;
+
+
+use Seat\Eveapi\Jobs\EsiBase;
+use Seat\Eveapi\Models\Character\CharacterTitle;
+
+/**
+ * Class Title
+ * @package Seat\Eveapi\Jobs\Character
+ */
+class Titles extends EsiBase
+{
+    /**
+     * @var string
+     */
+    protected $method = 'get';
+
+    /**
+     * @var string
+     */
+    protected $endpoint = '/characters/{character_id}/titles';
+
+    /**
+     * @var int
+     */
+    protected $version = 'v1';
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     * @throws \Exception
+     */
+    public function handle()
+    {
+
+        $titles = $this->retrieve([
+            'character_id' => $this->getCharacterId(),
+        ]);
+
+        // This is a small enough list to just wipe and add
+        CharacterTitle::where('character_id', $this->getCharacterId())
+            ->delete();
+
+        // Re-add the updated titles for this character
+        collect($titles)->each(function ($title) {
+
+            CharacterTitle::create([
+                'character_id' => $this->getCharacterId(),
+                'title_id'     => $title->title_id,
+                'name'         => $title->name,
+            ]);
+        });
+    }
+}
---
+++ src/database/migrations/2018_01_05_070317_create_character_titles_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCharacterTitlesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+
+        Schema::create('character_titles', function (Blueprint $table) {
+
+            $table->integer('character_id');
+            $table->integer('title_id');
+            $table->string('name');
+
+            $table->index('character_id');
+            $table->index('title_id');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+
+        Schema::dropIfExists('character_titles');
+    }
+}
