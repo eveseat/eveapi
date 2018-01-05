c97d15ebcb9145a0bb4d87f63b50fab59362d0a7
Leon Jacobs <leonja511@gmail.com>
2018-01-05 17:57:43 +0200

    Add character mailing lists updater.

---
+++ src/database/migrations/2018_01_05_141454_create_mail_mailing_lists_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMailMailingListsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+
+        Schema::create('mail_mailing_lists', function (Blueprint $table) {
+
+            $table->integer('character_id');
+            $table->integer('mailing_list_id');
+            $table->string('name');
+
+            $table->index('character_id');
+            $table->index('mailing_list_id');
+           
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+
+        Schema::dropIfExists('mail_mailing_lists');
+    }
+}
