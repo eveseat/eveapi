5264648ccae699c19686b28dabfead7071677e6b
Leon Jacobs <leonja511@gmail.com>
2018-01-07 18:00:05 +0200

    Add character contracts items updater.

---
+++ src/Jobs/Contracts/Character/Items.php
@@ -0,0 +1,91 @@
+<?php
+
+/*
+ * This file is part of SeAT
+ *
+ * Copyright (C) 2015, 2016, 2017, 2018  Leon Jacobs
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+namespace Seat\Eveapi\Jobs\Contracts\Character;
+
+
+use Seat\Eveapi\Jobs\EsiBase;
+use Seat\Eveapi\Models\Contacts\CharacterContract;
+use Seat\Eveapi\Models\Contacts\ContractItem;
+
+/**
+ * Class Items
+ * @package Seat\Eveapi\Jobs\Contracts\Character
+ */
+class Items extends EsiBase
+{
+    /**
+     * @var string
+     */
+    protected $method = 'get';
+
+    /**
+     * @var string
+     */
+    protected $endpoint = '/characters/{character_id}/contracts/{contract_id}/items/';
+
+    /**
+     * @var string
+     */
+    protected $version = 'v1';
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+
+        $empty_contracts = CharacterContract::join('contract_details',
+            'character_contracts.contract_id', '=',
+            'contract_details.contract_id')
+            ->where('type', '<>', 'courier')
+            ->whereNotIn('character_contracts.contract_id', function ($query) {
+
+                $query->select('contract_id')
+                    ->from('contract_items');
+            })
+            ->pluck('character_contracts.contract_id');
+
+        $empty_contracts->each(function ($contract_id) {
+
+            $items = $this->retrieve([
+                'character_id' => $this->getCharacterId(),
+                'contract_id'  => $contract_id,
+            ]);
+
+            collect($items)->each(function ($item) use ($contract_id) {
+
+                ContractItem::firstOrCreate([
+                    'contract_id'  => $contract_id,
+                    'record_id'    => $item->record_id,
+                    'type_id'      => $item->type_id,
+                    'quantity'     => $item->quantity,
+                    'raw_quantity' => isset($item->raw_quantity) ? $item->raw_quantity : null,
+                    'is_singleton' => $item->is_singleton,
+                    'is_included'  => $item->is_included,
+                ]);
+            });
+        });
+    }
+}
---
+++ src/database/migrations/2018_01_07_153340_create_contract_items_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateContractItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+
+        Schema::create('contract_items', function (Blueprint $table) {
+
+            $table->integer('contract_id');
+            $table->bigInteger('record_id');
+            $table->integer('type_id');
+            $table->integer('quantity');
+            $table->integer('raw_quantity')->nullable();
+            $table->boolean('is_singleton');
+            $table->boolean('is_included');
+
+            $table->primary(['contract_id', 'record_id']);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+
+        Schema::dropIfExists('contract_items');
+    }
+}
