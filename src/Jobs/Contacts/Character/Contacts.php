738c814ab61603573737b79a278490c92025de1b
Leon Jacobs <leonja511@gmail.com>
2018-01-07 14:14:45 +0200

    Add character contacts updater.

---
+++ src/Jobs/Contacts/Character/Contacts.php
@@ -0,0 +1,115 @@
+<?php
+
+/*
+ * This file is part of SeAT
+ *
+ * Copyright (C) 2015, 2016, 2017, 2018  Leon Jacobs
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+namespace Seat\Eveapi\Jobs\Contacts\Character;
+
+
+use Seat\Eveapi\Jobs\EsiBase;
+use Seat\Eveapi\Models\Contacts\CharacterContact;
+use Seat\Eveapi\Models\RefreshToken;
+
+/**
+ * Class Contacts
+ * @package Seat\Eveapi\Jobs\Contacts\Character
+ */
+class Contacts extends EsiBase
+{
+    /**
+     * @var string
+     */
+    protected $method = 'get';
+
+    /**
+     * @var string
+     */
+    protected $endpoint = '/characters/{character_id}/contacts/';
+
+    /**
+     * @var string
+     */
+    protected $version = 'v1';
+
+    /**
+     * @var int
+     */
+    protected $page = 1;
+
+    /**
+     * @var \Illuminate\Support\Collection
+     */
+    protected $known_contact_ids;
+
+    /**
+     * Contacts constructor.
+     *
+     * @param \Seat\Eveapi\Models\RefreshToken|null $token
+     */
+    public function __construct(RefreshToken $token = null)
+    {
+
+        $this->known_contact_ids = collect();
+
+        parent::__construct($token);
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     * @throws \Exception
+     */
+    public function handle()
+    {
+
+        while (true) {
+
+            $contacts = $this->retrieve([
+                'character_id' => $this->getCharacterId(),
+            ]);
+
+            collect($contacts)->each(function ($contact) {
+
+                CharacterContact::firstOrNew([
+                    'character_id' => $this->getCharacterId(),
+                    'contact_id'   => $contact->contact_id,
+                ])->fill([
+                    'standing'     => $contact->standing,
+                    'contact_type' => $contact->contact_type,
+                    'is_watched'   => isset($contact->is_watched) ? $contact->is_watched : false,
+                    'is_blocked'   => isset($contact->is_blocked) ? $contact->is_blocked : false,
+                    'label_id'     => isset($contact->label_id) ? $contact->label_id : null,
+                ])->save();
+            });
+
+            $this->known_contact_ids->push(collect($contacts)
+                ->pluck('contact_id')->flatten()->all());
+
+            if (! $this->nextPage($contacts->pages))
+                break;
+        }
+
+        // Cleanup old contacts
+        CharacterContact::where('character_id', $this->getCharacterId())
+            ->whereNotIn('contact_id', $this->known_contact_ids->flatten()->all())
+            ->delete();
+    }
+}
---
+++ src/database/migrations/2018_01_07_115737_create_character_contacts_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCharacterContactsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+
+        Schema::create('character_contacts', function (Blueprint $table) {
+
+            $table->integer('character_id');
+            $table->integer('contact_id');
+            $table->float('standing');
+            $table->enum('contact_type',
+                ['character', 'corporation', 'alliance', 'faction']);
+            $table->boolean('is_watched')->nullable();
+            $table->boolean('is_blocked')->nullable();
+            $table->bigInteger('label_id')->nullable();
+
+            $table->primary(['character_id', 'contact_id']);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+
+        Schema::dropIfExists('character_contacts');
+    }
+}
