1de19da6d840e28fd552eaa59d5a876997ffcf76
Leon Jacobs <leonja511@gmail.com>
2018-01-07 13:48:37 +0200

    Add character implants updater.

---
+++ src/database/migrations/2018_01_07_113945_create_character_implants_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCharacterImplantsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+
+        Schema::create('character_implants', function (Blueprint $table) {
+
+            $table->integer('character_id');
+            $table->integer('type_id');
+
+            $table->primary(['character_id', 'type_id']);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+
+        Schema::dropIfExists('character_implants');
+    }
+}
