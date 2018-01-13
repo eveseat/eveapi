e5bbbe50918a07498042120af33d01b5f1c9b147
Leon Jacobs <leonja511@gmail.com>
2018-01-13 09:32:28 +0200

    Move character skills updater to different namespace.

--- src/Jobs/Skills/Character/Skills.php
+++ src/Jobs/Skills/Character/Skills.php
@@ -20,7 +20,7 @@
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
-namespace Seat\Eveapi\Jobs\Character;
+namespace Seat\Eveapi\Jobs\Skills\Character;
 
 
 use Seat\Eveapi\Jobs\EsiBase;
