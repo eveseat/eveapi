91f27fdcb25a667f34de6d3587c34e5d3159f0b4
Leon Jacobs <leonja511@gmail.com>
2018-01-20 11:28:25 +0200

    Add corporation mining extractions updater.

---
+++ src/Jobs/Industry/Industry/MiningExtractions.php
@@ -0,0 +1,81 @@
+<?php
+
+/*
+ * This file is part of SeAT
+ *
+ * Copyright (C) 2015, 2016, 2017, 2018  Leon Jacobs
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+namespace Seat\Eveapi\Jobs\Industry\Corporation;
+
+
+use Seat\Eveapi\Jobs\EsiBase;
+use Seat\Eveapi\Models\Industry\CorporationIndustryMiningExtraction;
+
+/**
+ * Class MiningExtractions
+ * @package Seat\Eveapi\Jobs\Industry\Corporation
+ */
+class MiningExtractions extends EsiBase
+{
+    /**
+     * @var string
+     */
+    protected $method = 'get';
+
+    /**
+     * @var string
+     */
+    protected $endpoint = '/corporation/{corporation_id}/mining/extractions/';
+
+    /**
+     * @var string
+     */
+    protected $version = 'v1';
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     * @throws \Exception
+     */
+    public function handle()
+    {
+
+        $mining_extractions = $this->retrieve([
+            'corporation_id' => $this->getCorporationId(),
+        ]);
+
+        collect($mining_extractions)->each(function ($extraction) {
+
+            CorporationIndustryMiningExtraction::firstOrNew([
+                'corporation_id' => $this->getCorporationId(),
+                'structure_id'   => $extraction->structure_id,
+            ])->fill([
+                'moon_id'               => $extraction->moon_id,
+                'extraction_start_time' => carbon($extraction->extraction_start_time),
+                'chunk_arrival_time'    => carbon($extraction->chunk_arrival_time),
+                'natural_decay_time'    => carbon($extraction->natural_decay_time),
+            ])->save();
+        });
+
+        CorporationIndustryMiningExtraction::where('corporation_id', $this->getCorporationId())
+            ->whereNotIn('structure_id', collect($mining_extractions)
+                ->pluck('structure_id')->flatten()->all())
+            ->delete();
+    }
+}
---
+++ src/database/migrations/2018_01_20_092051_create_corporation_industry_mining_extractions_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCorporationIndustryMiningExtractionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+
+        Schema::create('corporation_industry_mining_extractions', function (Blueprint $table) {
+
+            $table->bigInteger('corporation_id');
+            $table->bigInteger('structure_id');
+            $table->integer('moon_id');
+            $table->dateTime('extraction_start_time');
+            $table->dateTime('chunk_arrival_time');
+            $table->dateTime('natural_decay_time');
+
+            $table->primary(['corporation_id', 'structure_id'], 'extrations_primary');
+            $table->index('corporation_id');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+
+        Schema::dropIfExists('corporation_industry_mining_extractions');
+    }
+}
