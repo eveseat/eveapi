b0f60fd934a1f0a5f6628420a0cbe4d79f6453dc
Leon Jacobs <leonja511@gmail.com>
2018-05-03 18:35:49 +0200

    Add relationships.

--- src/Models/Contracts/CorporationContract.php
+++ src/Models/Contracts/CorporationContract.php
@@ -42,4 +42,13 @@ class CorporationContract extends Model
      * @var array
      */
     protected $primaryKey = ['corporation_id', 'contract_id'];
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasOne
+     */
+    public function detail()
+    {
+
+        return $this->hasOne(ContractDetail::class, 'contract_id', 'contract_id');
+    }
 }
