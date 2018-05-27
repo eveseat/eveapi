9c1b7e9628435b07993248a7d5d2bfe64d7e80d7
Leon Jacobs <leonja511@gmail.com>
2018-05-27 17:12:19 +0200

    Add relationships for model cleanups.

--- src/Models/Character/CharacterInfo.php
+++ src/Models/Character/CharacterInfo.php
@@ -23,8 +23,28 @@
 namespace Seat\Eveapi\Models\Character;
 
 use Illuminate\Database\Eloquent\Model;
+use Seat\Eveapi\Models\Assets\CharacterAsset;
+use Seat\Eveapi\Models\Bookmarks\CharacterBookmark;
+use Seat\Eveapi\Models\Bookmarks\CharacterBookmarkFolder;
+use Seat\Eveapi\Models\Calendar\CharacterCalendarEvent;
+use Seat\Eveapi\Models\Clones\CharacterClone;
+use Seat\Eveapi\Models\Clones\CharacterImplant;
+use Seat\Eveapi\Models\Clones\CharacterJumpClone;
+use Seat\Eveapi\Models\Contacts\CharacterContact;
+use Seat\Eveapi\Models\Contacts\CharacterContactLabel;
+use Seat\Eveapi\Models\Contacts\CharacterFitting;
+use Seat\Eveapi\Models\Contracts\CharacterContract;
+use Seat\Eveapi\Models\Industry\CharacterIndustryJob;
+use Seat\Eveapi\Models\Industry\CharacterMining;
+use Seat\Eveapi\Models\Killmails\CharacterKillmail;
+use Seat\Eveapi\Models\Location\CharacterLocation;
+use Seat\Eveapi\Models\Location\CharacterOnline;
 use Seat\Eveapi\Models\Location\CharacterShip;
+use Seat\Eveapi\Models\Market\CharacterOrder;
+use Seat\Eveapi\Models\Skills\CharacterAttribute;
 use Seat\Eveapi\Models\Wallet\CharacterWalletBalance;
+use Seat\Eveapi\Models\Wallet\CharacterWalletJournal;
+use Seat\Eveapi\Models\Wallet\CharacterWalletTransaction;
 use Seat\Services\Traits\NotableTrait;
 
 /**
@@ -46,6 +66,76 @@ class CharacterInfo extends Model
     protected $primaryKey = 'character_id';
 
     /**
+     * Make sure we cleanup on delete.
+     *
+     * @return bool|null
+     * @throws \Exception
+     */
+    public function delete()
+    {
+
+        // Cleanup the character
+        $this->agent_research()->delete();
+        $this->assets()->delete();
+        $this->balance()->delete();
+        $this->blueprints()->delete();
+        $this->bookmarks()->delete();
+        $this->bookmark_folders()->delete();
+        $this->calendar_events()->delete();
+        $this->clones()->delete();
+        $this->contact_labels()->delete();
+        $this->contacts()->delete();
+        $this->contracts()->delete();
+        $this->corporation_history()->delete();
+        $this->fatigue()->delete();
+        $this->fittings()->delete();
+        $this->implants()->delete();
+        $this->industry()->delete();
+        $this->jump_clones()->delete();
+        $this->killmails()->delete();
+        $this->location()->delete();
+        $this->medals()->delete();
+        $this->mining()->delete();
+        $this->notifications()->delete();
+        $this->online()->delete();
+        $this->orders()->delete();
+
+        // TODO: PI Cleanup
+
+        $this->standings()->delete();
+        $this->stats()->delete();
+        $this->titles()->delete();
+        $this->corporation_roles()->delete();
+        $this->pilot_attributes()->delete();
+        $this->skills()->delete();
+        $this->ship()->delete();
+        $this->wallet_journal()->delete();
+        $this->wallet_transactions()->delete();
+
+        return parent::delete();
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function agent_research()
+    {
+
+        return $this->hasMany(CharacterAgentResearch::class,
+            'character_id', 'character_id');
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function assets()
+    {
+
+        return $this->hasMany(CharacterAsset::class,
+            'character_id', 'character_id');
+    }
+
+    /**
      * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
      */
     public function balance()
@@ -56,23 +146,83 @@ class CharacterInfo extends Model
     }
 
     /**
-     * @return mixed
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
      */
-    public function corporation()
+    public function blueprints()
     {
 
-        return CharacterCorporationHistory::where('character_id', $this->character_id)
-            ->orderBy('record_id', 'desc')
-            ->first();
+        return $this->hasMany(CharacterBlueprint::class,
+            'character_id', 'character_id');
     }
 
     /**
      * @return \Illuminate\Database\Eloquent\Relations\HasMany
      */
-    public function corporation_roles()
+    public function bookmarks()
     {
 
-        return $this->hasMany(CharacterRole::class, 'character_id', 'character_id');
+        return $this->hasMany(CharacterBookmark::class,
+            'character_id', 'character_id');
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function bookmark_folders()
+    {
+
+        return $this->hasMany(CharacterBookmarkFolder::class,
+            'character_id', 'character_id');
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function calendar_events()
+    {
+
+        return $this->hasMany(CharacterCalendarEvent::class,
+            'character_id', 'character_id');
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function clones()
+    {
+
+        return $this->hasMany(CharacterClone::class,
+            'character_id', 'character_id');
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function contact_labels()
+    {
+
+        return $this->hasMany(CharacterContactLabel::class,
+            'character_id', 'character_id');
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function contacts()
+    {
+
+        return $this->hasMany(CharacterContact::class,
+            'character_id', 'character_id');
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function contracts()
+    {
+
+        return $this->hasMany(CharacterContract::class,
+            'character_id', 'character_id');
     }
 
     /**
@@ -82,6 +232,176 @@ class CharacterInfo extends Model
     {
 
         return $this->hasMany(CharacterCorporationHistory::class,
+            'character_id', 'character_id');
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasOne
+     */
+    public function fatigue()
+    {
+
+        return $this->hasOne(CharacterFatigue::class,
+            'character_id', 'character_id');
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function fittings()
+    {
+
+        return $this->hasMany(CharacterFitting::class,
+            'character_id', 'character_id');
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function implants()
+    {
+
+        return $this->hasMany(CharacterImplant::class,
+            'character_id', 'character_id');
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function industry()
+    {
+
+        return $this->hasMany(CharacterIndustryJob::class,
+            'character_id', 'character_id');
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function jump_clones()
+    {
+
+        return $this->hasMany(CharacterJumpClone::class,
+            'character_id', 'character_id');
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function killmails()
+    {
+
+        return $this->hasMany(CharacterKillmail::class,
+            'character_id', 'character_id');
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasOne
+     */
+    public function location()
+    {
+
+        return $this->hasOne(CharacterLocation::class,
+            'character_id', 'character_id');
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function medals()
+    {
+
+        return $this->hasMany(CharacterMedal::class,
+            'character_id', 'character_id');
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function mining()
+    {
+
+        return $this->hasMany(CharacterMining::class,
+            'character_id', 'character_id');
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function notifications()
+    {
+
+        return $this->hasMany(CharacterNotification::class,
+            'character_id', 'character_id');
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasOne
+     */
+    public function online()
+    {
+
+        return $this->hasOne(CharacterOnline::class,
+            'character_id', 'character_id');
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function orders()
+    {
+
+        return $this->hasMany(CharacterOrder::class,
+            'character_id', 'character_id');
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function standings()
+    {
+
+        return $this->hasMany(CharacterStanding::class,
+            'character_id', 'character_id');
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function stats()
+    {
+
+        return $this->hasMany(CharacterStats::class,
+            'character_id', 'character_id');
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function titles()
+    {
+
+        return $this->hasMany(CharacterTitle::class,
+            'character_id', 'character_id');
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function corporation_roles()
+    {
+
+        return $this->hasMany(CharacterRole::class,
+            'character_id', 'character_id');
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function pilot_attributes()
+    {
+
+        return $this->hasMany(CharacterAttribute::class,
             'character_id', 'character_id');
     }
 
@@ -101,6 +421,38 @@ class CharacterInfo extends Model
     public function ship()
     {
 
-        return $this->hasOne(CharacterShip::class, 'character_id', 'character_id');
+        return $this->hasOne(CharacterShip::class,
+            'character_id', 'character_id');
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function wallet_journal()
+    {
+
+        return $this->hasMany(CharacterWalletJournal::class,
+            'character_id', 'character_id');
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function wallet_transactions()
+    {
+
+        return $this->hasMany(CharacterWalletTransaction::class,
+            'character_id', 'character_id');
+    }
+
+    /**
+     * @return mixed
+     */
+    public function corporation()
+    {
+
+        return CharacterCorporationHistory::where('character_id', $this->character_id)
+            ->orderBy('record_id', 'desc')
+            ->first();
     }
 }
--- src/Models/Corporation/CorporationInfo.php
+++ src/Models/Corporation/CorporationInfo.php
@@ -23,7 +23,20 @@
 namespace Seat\Eveapi\Models\Corporation;
 
 use Illuminate\Database\Eloquent\Model;
+use Seat\Eveapi\Models\Assets\CorporationAsset;
+use Seat\Eveapi\Models\Bookmarks\CorporationBookmark;
+use Seat\Eveapi\Models\Bookmarks\CorporationBookmarkFolder;
+use Seat\Eveapi\Models\Contacts\CorporationContact;
+use Seat\Eveapi\Models\Contacts\CorporationContactLabel;
+use Seat\Eveapi\Models\Contracts\CorporationContract;
+use Seat\Eveapi\Models\Industry\CorporationIndustryJob;
+use Seat\Eveapi\Models\Killmails\CorporationKillmail;
+use Seat\Eveapi\Models\Market\CorporationOrder;
+use Seat\Eveapi\Models\PlanetaryInteraction\CorporationCustomsOffice;
 use Seat\Eveapi\Models\Universe\UniverseStation;
+use Seat\Eveapi\Models\Wallet\CorporationWalletBalance;
+use Seat\Eveapi\Models\Wallet\CorporationWalletJournal;
+use Seat\Eveapi\Models\Wallet\CorporationWalletTransaction;
 
 /**
  * Class CorporationInfo.
@@ -47,20 +60,396 @@ class CorporationInfo extends Model
     protected $primaryKey = 'corporation_id';
 
     /**
-     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     * Make sure we cleanup on delete.
+     *
+     * @return bool|null
+     * @throws \Exception
      */
-    public function homeStation()
+    public function delete()
     {
 
-        return $this->belongsTo(UniverseStation::class, 'home_station_id', 'station_id');
+        // Cleanup the corporation
+        $this->alliance_history()->delete();
+        $this->assets()->delete();
+        $this->blueprints()->delete();
+        $this->bookmarks()->delete();
+        $this->bookmark_folders()->delete();
+        $this->contacts()->delete();
+        $this->contact_labels()->delete();
+        $this->container_logs()->delete();
+        $this->contracts()->delete();
+        $this->pocos()->delete();
+        $this->divisions()->delete();
+        $this->facilities()->delete();
+        $this->industry_jobs()->delete();
+
+        // TODO: Mining delete
+
+        $this->issued_medals()->delete();
+        $this->killmails()->delete();
+        $this->medals()->delete();
+        $this->member_limit()->delete();
+        $this->member_titles()->delete();
+        $this->member_tracking()->delete();
+        $this->members()->delete();
+        $this->orders()->delete();
+        $this->outposts()->delete();
+        $this->outpost_details()->delete();
+        $this->outpost_services()->delete();
+        $this->roles()->delete();
+        $this->role_history()->delete();
+        $this->shareholders()->delete();
+        $this->standings()->delete();
+
+        // TODO: Starbases & Structures
+
+        $this->titles()->delete();
+        $this->title_roles()->delete();
+        $this->wallet_balances()->delete();
+        $this->wallet_journal()->delete();
+        $this->wallet_transactions()->delete();
+
+        return parent::delete();
     }
 
     /**
-     * @return int|null
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
      */
-    public function getAttributeMemberLimit()
+    public function alliance_history()
     {
 
-        return optional(CorporationMemberLimits::find($this->corporation_id))->limit;
+        return $this->hasMany(CorporationAllianceHistory::class,
+            'corporation_id', 'corporation_id');
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function assets()
+    {
+
+        return $this->hasMany(CorporationAsset::class,
+            'corporation_id', 'corporation_id');
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function blueprints()
+    {
+
+        return $this->hasMany(CorporationBlueprint::class,
+            'corporation_id', 'corporation_id');
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function bookmarks()
+    {
+
+        return $this->hasMany(CorporationBookmark::class,
+            'corporation_id', 'corporation_id');
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function bookmark_folders()
+    {
+
+        return $this->hasMany(CorporationBookmarkFolder::class,
+            'corporation_id', 'corporation_id');
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function contacts()
+    {
+
+        return $this->hasMany(CorporationContact::class,
+            'corporation_id', 'corporation_id');
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function contact_labels()
+    {
+
+        return $this->hasMany(CorporationContactLabel::class,
+            'corporation_id', 'corporation_id');
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function container_logs()
+    {
+
+        return $this->hasMany(CorporationContainerLog::class,
+            'corporation_id', 'corporation_id');
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function contracts()
+    {
+
+        return $this->hasMany(CorporationContract::class,
+            'corporation_id', 'corporation_id');
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function pocos()
+    {
+
+        return $this->hasMany(CorporationCustomsOffice::class,
+            'corporation_id', 'corporation_id');
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function divisions()
+    {
+
+        return $this->hasMany(CorporationDivision::class,
+            'corporation_id', 'corporation_id');
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function facilities()
+    {
+
+        return $this->hasMany(CorporationFacility::class,
+            'corporation_id', 'corporation_id');
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function industry_jobs()
+    {
+
+        return $this->hasMany(CorporationIndustryJob::class,
+            'corporation_id', 'corporation_id');
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function issued_medals()
+    {
+
+        return $this->hasMany(CorporationIssuedMedal::class,
+            'corporation_id', 'corporation_id');
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function killmails()
+    {
+
+        return $this->hasMany(CorporationKillmail::class,
+            'corporation_id', 'corporation_id');
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function medals()
+    {
+
+        return $this->hasMany(CorporationMedal::class,
+            'corporation_id', 'corporation_id');
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasOne
+     */
+    public function member_limit()
+    {
+
+
+        return $this->hasOne(CorporationMemberLimits::class,
+            'corporation_id', 'corporation_id');
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function member_titles()
+    {
+
+        return $this->hasMany(CorporationMemberTitle::class,
+            'corporation_id', 'corporation_id');
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function member_tracking()
+    {
+
+        return $this->hasMany(CorporationMemberTracking::class,
+            'corporation_id', 'corporation_id');
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function members()
+    {
+
+        return $this->hasMany(CorporationMember::class,
+            'corporation_id', 'corporation_id');
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function orders()
+    {
+
+        return $this->hasMany(CorporationOrder::class,
+            'corporation_id', 'corporation_id');
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function outposts()
+    {
+
+        return $this->hasMany(CorporationOutpost::class,
+            'corporation_id', 'corporation_id');
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function outpost_details()
+    {
+
+        return $this->hasMany(CorporationOutpostDetail::class,
+            'corporation_id', 'corporation_id');
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function outpost_services()
+    {
+
+        return $this->hasMany(CorporationOutpostService::class,
+            'corporation_id', 'corporation_id');
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function roles()
+    {
+
+        return $this->hasMany(CorporationRole::class,
+            'corporation_id', 'corporation_id');
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function role_history()
+    {
+
+        return $this->hasMany(CorporationRoleHistory::class,
+            'corporation_id', 'corporation_id');
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function shareholders()
+    {
+
+        return $this->hasMany(CorporationShareholder::class,
+            'corporation_id', 'corporation_id');
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function standings()
+    {
+
+        return $this->hasMany(CorporationStanding::class,
+            'corporation_id', 'corporation_id');
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function titles()
+    {
+
+        return $this->hasMany(CorporationTitle::class,
+            'corporation_id', 'corporation_id');
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function title_roles()
+    {
+
+        return $this->hasMany(CorporationTitleRole::class,
+            'corporation_id', 'corporation_id');
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function wallet_balances()
+    {
+
+        return $this->hasMany(CorporationWalletBalance::class,
+            'corporation_id', 'corporation_id');
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function wallet_journal()
+    {
+
+        return $this->hasMany(CorporationWalletJournal::class,
+            'corporation_id', 'corporation_id');
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function wallet_transactions()
+    {
+
+        return $this->hasMany(CorporationWalletTransaction::class,
+            'corporation_id', 'corporation_id');
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function home_station()
+    {
+
+        return $this->belongsTo(UniverseStation::class,
+            'home_station_id', 'station_id');
     }
 }
