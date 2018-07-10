db43e5dc24f72b28d6cd5c679ebef2b91793920b
Loïc LEUILLIOT <e.elfaus@gmail.com>
2018-07-10 02:54:33 +0200

    Generate complete documentation for corporation category (HTTP/200)

--- src/Models/Corporation/CorporationMemberTracking.php
+++ src/Models/Corporation/CorporationMemberTracking.php
@@ -29,6 +29,148 @@ use Seat\Eveapi\Traits\HasCompositePrimaryKey;
 /**
  * Class CorporationMemberTracking.
  * @package Seat\Eveapi\Models\Corporation
+ *
+ * @SWG\Definition(
+ *      description="Corporation Member Tracking",
+ *      title="CorporationMemberTracking",
+ *      type="object"
+ * )
+ *
+ * @SWG\Property(
+ *     type="integer",
+ *     format="int64",
+ *     property="character_id",
+ *     description="The character ID"
+ * )
+ *
+ * @SWG\Property(
+ *     type="string",
+ *     format="date-time",
+ *     property="start_date",
+ *     description="The date since which the character is member of the corporation"
+ * )
+ *
+ * @SWG\Property(
+ *     type="integer",
+ *     format="int64",
+ *     property="base_id",
+ *     description="The structure to which the main location of this character is set"
+ * )
+ *
+ * @SWG\Property(
+ *     type="string",
+ *     format="date-time",
+ *     property="logon_date",
+ *     description="The last time when we saw the character"
+ * )
+ *
+ * @SWG\Property(
+ *     type="string",
+ *     format="date-time",
+ *     property="logoff_time",
+ *     description="The last time when the character signed out"
+ * )
+ *
+ * @SWG\Property(
+ *     type="integer",
+ *     format="int64",
+ *     property="location_id",
+ *     description="The place where the character is"
+ * )
+ *
+ * @SWG\Property(
+ *     type="integer",
+ *     property="ship_type_id",
+ *     description="The typeID of the ship into which the character is in"
+ * )
+ *
+ * @SWG\Property(
+ *     type="object",
+ *     property="ship_type",
+ *     description="The ship information",
+ *     @SWG\Property(
+ *          type="integer",
+ *          property="typeID",
+ *          description="The type ID of the ship"
+ *     ),
+ *     @SWG\Property(
+ *          type="integer",
+ *          property="groupID",
+ *          description="The group ID of the ship"
+ *     ),
+ *     @SWG\Property(
+ *          type="string",
+ *          property="typeName",
+ *          description="The name of the ship (item)"
+ *     ),
+ *     @SWG\Property(
+ *          type="string",
+ *          property="description",
+ *          description="The description of the ship"
+ *     ),
+ *     @SWG\Property(
+ *          type="number",
+ *          format="double",
+ *          property="mass",
+ *          description="The mass of the ship"
+ *     ),
+ *     @SWG\Property(
+ *          type="number",
+ *          format="double",
+ *          property="volume",
+ *          description="The volume of the ship"
+ *     ),
+ *     @SWG\Property(
+ *          type="number",
+ *          format="double",
+ *          property="capacity",
+ *          description="The cargo capacity of the ship"
+ *     ),
+ *     @SWG\Property(
+ *          type="integer",
+ *          property="portionSize",
+ *          description="The portion size of the ship"
+ *     ),
+ *     @SWG\Property(
+ *          type="integer",
+ *          property="raceID",
+ *          description="The ID of the race to which the asset is related"
+ *     ),
+ *     @SWG\Property(
+ *          type="number",
+ *          format="float",
+ *          property="basePrice",
+ *          description="The initial price as state by CCP"
+ *     ),
+ *     @SWG\Property(
+ *          type="boolean",
+ *          property="published",
+ *          description="Determine if the item is available in-game"
+ *     ),
+ *     @SWG\Property(
+ *          type="integer",
+ *          property="marketGroupID",
+ *          description="The ID of the group on the market"
+ *     ),
+ *     @SWG\Property(
+ *          type="number",
+ *          format="double",
+ *          property="iconID",
+ *          description="The asset icon ID"
+ *     ),
+ *     @SWG\Property(
+ *          type="number",
+ *          format="double",
+ *          property="soundID",
+ *          description="The asset sound ID"
+ *     ),
+ *     @SWG\Property(
+ *          type="number",
+ *          format="double",
+ *          property="graphicID",
+ *          description="The asset graphic ID"
+ *     )
+ * )
  */
 class CorporationMemberTracking extends Model
 {
