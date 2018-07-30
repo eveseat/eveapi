46c5c5a9dc92252f7fb620ddd9fa95df840c9c21
Loïc LEUILLIOT <e.elfaus@gmail.com>
2018-07-30 22:30:07 +0200

    fix(universe structure): add missing corporation locations
    Refactor the job and append the corporation locations which have been omit.
    
    Closes eveseat/seat#337

--- src/Jobs/Universe/Structures.php
+++ src/Jobs/Universe/Structures.php
@@ -25,6 +25,7 @@ namespace Seat\Eveapi\Jobs\Universe;
 use Seat\Eseye\Exceptions\RequestFailedException;
 use Seat\Eveapi\Jobs\EsiBase;
 use Seat\Eveapi\Models\Assets\CharacterAsset;
+use Seat\Eveapi\Models\Assets\CorporationAsset;
 use Seat\Eveapi\Models\Sde\StaStation;
 use Seat\Eveapi\Models\Universe\UniverseStation;
 use Seat\Eveapi\Models\Universe\UniverseStructure;
@@ -64,7 +65,7 @@ class Structures extends EsiBase
     /**
      * @var string
      */
-    protected $version = 'v1';
+    protected $version = 'v2';
 
     /**
      * @var array
@@ -83,7 +84,72 @@ class Structures extends EsiBase
 
         if (! $this->preflighted()) return;
 
-        $character_assets = CharacterAsset::where('character_id', $this->getCharacterId())
+        // retrieve unresolved location from character
+        $character_asset_locations = $this->getCharacterAssetLocations();
+
+        // retrieve unresolved location from corporation
+        $corporation_asset_locations = $this->getCorporationAssetLocations($character_asset_locations);
+
+        // merge both character and corporation arrays
+        $location_ids = array_merge($character_asset_locations, $corporation_asset_locations);
+
+        foreach ($location_ids as $location_id) {
+
+            try {
+
+                // If we are rate limited, stop working.
+                if ($this->isRateLimited()) break;
+
+                $structure = $this->retrieve([
+                    'structure_id' => $location_id,
+                ]);
+
+                // Increment the call count we have this far.
+                $this->incrementRateLimitCallCount(1);
+
+                UniverseStructure::firstOrNew([
+                    'structure_id' => $location_id,
+                ], [
+                    'name'            => $structure->name,
+                    'owner_id'        => $structure->owner_id,
+                    'solar_system_id' => $structure->solar_system_id,
+                    'x'               => $structure->position->x,
+                    'y'               => $structure->position->y,
+                    'z'               => $structure->position->z,
+                    'type_id'         => property_exists($structure, 'type_id') ? $structure->type_id : null,
+                ])->save();
+
+            } catch (RequestFailedException $e) {
+
+                // Failure to grab the structure should result in us creating an
+                // empty entry in the database for this structure.
+
+                $model = UniverseStructure::firstOrNew([
+                    'structure_id' => $location_id,
+                ])->fill([
+                    'name'            => 'Unknown Structure',
+                    'owner_id'        => null,
+                    'solar_system_id' => 0,
+                    'x'               => 0.0,
+                    'y'               => 0.0,
+                    'z'               => 0.0,
+                    'type_id'         => null,
+                ]);
+
+                // persist the structure only if it doesn't already exist
+                if (! $model->exists)
+                    $model->save();
+            }
+        }
+    }
+
+    /**
+     * @return array
+     * @throws \Exception
+     */
+    private function getCharacterAssetLocations() : array
+    {
+        $assets = CharacterAsset::where('character_id', $this->getCharacterId())
             ->where('location_flag', 'Hangar')
             ->where('location_type', 'other')
             ->whereNotIn('location_id', function ($query) {
@@ -102,6 +168,7 @@ class Structures extends EsiBase
 
                 $query->select('structure_id')
                     ->from((new UniverseStructure)->getTable())
+                    ->where('name', '<>', 'Unknown structure')
                     ->where('updated_at', '<', carbon('now')->subWeek());
             })
             ->select('location_id')->distinct()
@@ -112,53 +179,54 @@ class Structures extends EsiBase
             ->limit(30)
             ->get();
 
-        foreach ($character_assets as $character_asset) {
+        return $assets->map(function ($asset, $key) {
+            return $asset->location_id;
+        })->all();
+    }
 
-            try {
+    /**
+     * @return array
+     * @throws \Exception
+     */
+    private function getCorporationAssetLocations(array $exclude_location_ids = []) : array
+    {
+        $assets = CorporationAsset::where('corporation_id', $this->getCorporationId())
+            ->whereIn('location_flag', ['OfficeFolder', 'CorpDeliveries'])
+            ->where('location_type', 'other')
+            // ignore already listed location_id
+            ->whereNotIn('location_id', $exclude_location_ids)
+            // ignore already known station
+            ->whereNotIn('location_id', function ($query) {
 
-                // If we are rate limited, stop working.
-                if ($this->isRateLimited()) break;
+                $query->select('station_id')
+                    ->from((new UniverseStation)->getTable());
+            })
+            // ignore outpost [DEPRECATED ?]
+            ->whereNotIn('location_id', function ($query) {
 
-                $structure = $this->retrieve([
-                    'structure_id' => $character_asset->location_id,
-                ]);
+                $query->select('stationID')
+                    ->from((new StaStation)->getTable());
+            })
+            // Remove structures that already have a name resolved
+            // within the last week.
+            ->whereNotIn('location_id', function ($query) {
 
-                // Increment the call count we have this far.
-                $this->incrementRateLimitCallCount(1);
+                $query->select('structure_id')
+                    ->from((new UniverseStructure)->getTable())
+                    ->where('name', '<>', 'Unknown structure')
+                    ->where('updated_at', '<', carbon('now')->subWeek());
+            })
+            ->select('location_id')
+            ->distinct()
+            // Until CCP can sort out this endpoint, pick 30 random locations
+            // and try to get those names. We hard cap it at 30 otherwise we
+            // will quickly kill the error limit, resulting in a ban.
+            ->inRandomOrder()
+            ->limit(30)
+            ->get();
 
-                UniverseStructure::firstOrNew([
-                    'structure_id' => $character_asset->location_id,
-                ], [
-                    'name'            => $structure->name,
-                    'owner_id'        => $structure->owner_id,
-                    'solar_system_id' => $structure->solar_system_id,
-                    'x'               => $structure->position->x,
-                    'y'               => $structure->position->y,
-                    'z'               => $structure->position->z,
-                    'type_id'         => property_exists($structure, 'type_id') ? $structure->type_id : null,
-                ])->save();
-
-            } catch (RequestFailedException $e) {
-
-                // Failure to grab the structure should result in us creating an
-                // empty entry in the database for this structure.
-
-                $model = UniverseStructure::firstOrNew([
-                    'structure_id' => $character_asset->location_id,
-                ])->fill([
-                    'name'            => 'Unknown Structure',
-                    'owner_id'        => null,
-                    'solar_system_id' => 0,
-                    'x'               => 0.0,
-                    'y'               => 0.0,
-                    'z'               => 0.0,
-                    'type_id'         => null,
-                ]);
-
-                // persist the structure only if it doesn't already exist
-                if (! $model->exists)
-                    $model->save();
-            }
-        }
+        return $assets->map(function ($asset, $key) {
+            return $asset->location_id;
+        })->all();
     }
 }
