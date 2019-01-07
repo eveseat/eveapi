38c363417b06e05c20ce44ef064ded17f30c0a17
Loïc Leuilliot <e.elfaus@gmail.com>
2019-01-07 20:22:42 +0100

    feat(versions): Implement new version management for EveAPI package (#137)
    * feat(versions): Implement new version management for EveAPI package
    
    * refactor(versions): Avoid static definition
    
    * refactor(versions): Apply changes according to services review

--- src/EveapiServiceProvider.php
+++ src/EveapiServiceProvider.php
@@ -22,14 +22,14 @@
 
 namespace Seat\Eveapi;
 
-use Illuminate\Support\ServiceProvider;
 use Seat\Eveapi\Helpers\EseyeSetup;
+use Seat\Services\AbstractSeatPlugin;
 
 /**
  * Class EveapiServiceProvider.
  * @package Seat\Eveapi
  */
-class EveapiServiceProvider extends ServiceProvider
+class EveapiServiceProvider extends AbstractSeatPlugin
 {
     /**
      * Bootstrap the application services.
@@ -91,5 +91,55 @@ class EveapiServiceProvider extends ServiceProvider
                 __DIR__ . '/Models',
             ])),
         ]);
+    }
+
+    /**
+     * Return the plugin public name as it should be displayed into settings.
+     *
+     * @return string
+     */
+    public function getName(): string
+    {
+        return 'SeAT Eve API';
+    }
+
+    /**
+     * Return the plugin repository address.
+     *
+     * @return string
+     */
+    public function getPackageRepositoryUrl(): string
+    {
+        return 'https://github.com/eveseat/eveapi';
+    }
+
+    /**
+     * Return the plugin technical name as published on package manager.
+     *
+     * @return string
+     */
+    public function getPackagistPackageName(): string
+    {
+        return 'eveapi';
+    }
+
+    /**
+     * Return the plugin vendor tag as published on package manager.
+     *
+     * @return string
+     */
+    public function getPackagistVendorName(): string
+    {
+        return 'eveseat';
+    }
+
+    /**
+     * Return the plugin installed version.
+     *
+     * @return string
+     */
+    public function getVersion(): string
+    {
+        return config('eveapi.config.version');
     }
 }
