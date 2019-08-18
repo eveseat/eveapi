7a97d9f17674f725dd17a808aee4285b06275c08
Loïc LEUILLIOT <loic.leuilliot@gmail.com>
2019-08-18 10:40:46 +0200

    refactor(orders): add relation to invType and structures

--- src/Models/Market/CorporationOrder.php
+++ src/Models/Market/CorporationOrder.php
@@ -23,6 +23,8 @@
 namespace Seat\Eveapi\Models\Market;
 
 use Illuminate\Database\Eloquent\Model;
+use Seat\Eveapi\Models\Sde\InvType;
+use Seat\Eveapi\Models\Universe\UniverseStructure;
 use Seat\Eveapi\Traits\HasCompositePrimaryKey;
 
 /**
@@ -154,4 +156,27 @@ class CorporationOrder extends Model
      * @var array
      */
     protected $primaryKey = ['corporation_id', 'order_id'];
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasOne
+     */
+    public function location()
+    {
+        return $this->hasOne(UniverseStructure::class, 'structure_id', 'location_id')
+            ->withDefault([
+                'name' => trans('web::seat.unknown'),
+            ]);
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasOne
+     */
+    public function type()
+    {
+        return $this->hasOne(InvType::class, 'typeID', 'type_id')
+            ->withDefault([
+                'typeID'   => 0,
+                'typeName' => trans('web::seat.unknown'),
+            ]);
+    }
 }
