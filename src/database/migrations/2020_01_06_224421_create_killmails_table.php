5039efa022b23f9c3dce16792eb857f6ce04cb75
Loïc LEUILLIOT <loic.leuilliot@gmail.com>
2020-01-07 00:14:08 +0100

    refactor(jobs): merge corporation and character killmails
    in order to reduce data duplication and match with ESI design
    this commit is removing both corporation_killmails and
    character_killmails which were storing only killmail_id
    and killmail_hash collected from entities.
    
    killmail jobs has been updated to seed this new table instead.
    
    finally, we rollback to a job is equal to an action pattern.
    as a result, job responsible to collect killmail details is
    non longer run per entity and don't use any token.
    
    every time a killmail will be inserted, a job will be dispatched
    to collect related data (trigger might be replaced by an observer).

---
+++ src/database/migrations/2020_01_06_224421_create_killmails_table.php
@@ -0,0 +1,111 @@
+<?php
+
+/*
+ * This file is part of SeAT
+ *
+ * Copyright (C) 2015, 2016, 2017, 2018, 2019  Leon Jacobs
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+/**
+ * Class CreateKillmailsTable.
+ */
+class CreateKillmailsTable extends Migration
+{
+    public function up()
+    {
+        // spawn the new unified killmails table
+        Schema::create('killmails', function (Blueprint $table) {
+            $table->bigInteger('killmail_id');
+            $table->string('killmail_hash');
+
+            $table->timestamps();
+
+            $table->primary('killmail_id');
+        });
+
+        // collect mails from deprecated tables
+        $killmails = DB::table('character_killmails')
+            ->select('killmail_id', 'killmail_hash')
+            ->selectRaw('"2020-01-06 00:00:00" as created_at')
+            ->selectRaw('"2020-01-06 00:00:00" as updated_at')
+            ->union(
+                DB::table('corporation_killmails')
+                    ->select('killmail_id', 'killmail_hash')
+                    ->selectRaw('"2020-01-06 00:00:00" as created_at')
+                    ->selectRaw('"2020-01-06 00:00:00" as updated_at')
+            )
+            ->distinct();
+
+        // populate the new table with historical data
+        DB::table('killmails')
+            ->insertUsing(['killmail_id', 'killmail_hash', 'created_at', 'updated_at'], $killmails);
+
+        // fix existing foreign keys to cascade
+        Schema::table('killmail_attackers', function (Blueprint $table) {
+            $table->dropForeign(['killmail_id']);
+
+            $table->foreign('killmail_id')
+                ->references('killmail_id')
+                ->on('killmail_details')
+                ->onDelete('cascade');
+        });
+
+        Schema::table('killmail_victims', function (Blueprint $table) {
+            $table->dropForeign(['killmail_id']);
+
+            $table->foreign('killmail_id')
+                ->references('killmail_id')
+                ->on('killmail_details')
+                ->onDelete('cascade');
+        });
+
+        Schema::table('killmail_victim_items', function (Blueprint $table) {
+            $table->dropForeign(['killmail_id']);
+
+            $table->foreign('killmail_id')
+                ->references('killmail_id')
+                ->on('killmail_details')
+                ->onDelete('cascade');
+        });
+
+        // remove orphan killmails
+        DB::table('killmail_details')
+            ->whereNotIn('killmail_id', DB::table('killmails')->select('killmail_id'))
+            ->delete();
+
+        // add foreign key between killmail and header
+        Schema::table('killmail_details', function (Blueprint $table) {
+            $table->foreign('killmail_id')
+                ->references('killmail_id')
+                ->on('killmails');
+        });
+
+        // drop deprecated tables
+        Schema::drop('character_killmails');
+        Schema::drop('corporation_killmails');
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('killmails');
+    }
+}
