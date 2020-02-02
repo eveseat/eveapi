a89bec1ba32fde1feddbb318aa85027ef2b9421c
Loïc LEUILLIOT <loic.leuilliot@gmail.com>
2020-02-02 20:14:24 +0100

    refactor(surrogate): replace the surrogate pattern with a bigint key
    this cause troubles with serializing and jobs/notifications.

---
+++ src/database/migrations/2020_02_02_195740_add_id_to_corporation_member_trackings.php
@@ -0,0 +1,57 @@
+<?php
+
+/*
+ * This file is part of SeAT
+ *
+ * Copyright (C) 2015, 2016, 2017, 2018, 2019, 2020  Leon Jacobs
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+/**
+ * Class AddIdToCorporationMemberTrackings.
+ */
+class AddIdToCorporationMemberTrackings extends Migration
+{
+    public function up()
+    {
+        Schema::table('corporation_member_trackings', function (Blueprint $table) {
+            $table->dropPrimary(['corporation_id', 'character_id']);
+        });
+
+        Schema::table('corporation_member_trackings', function (Blueprint $table) {
+            $table->bigIncrements('id')->first();
+
+            $table->unique(['corporation_id', 'character_id']);
+        });
+    }
+
+    public function down()
+    {
+        Schema::table('corporation_member_trackings', function (Blueprint $table) {
+            $table->dropPrimary('id');
+
+            $table->dropUnique(['corporation_id', 'character_id']);
+        });
+
+        Schema::table('corporation_member_trackings', function (Blueprint $table) {
+            $table->primary(['corporation_id', 'character_id']);
+        });
+    }
+}
