c84c86d7445def2c906d798a8ad577c7ca99ac69
Loïc LEUILLIOT <loic.leuilliot@gmail.com>
2020-02-06 22:46:21 +0100

    refactor: enqueue character and corporation jobs on new token

---
+++ src/Observers/CharacterAffiliationObserver.php
@@ -0,0 +1,49 @@
+<?php
+
+/*
+ * This file is part of SeAT
+ *
+ * Copyright (C) 2015, 2016, 2017, 2018, 2019  Leon Jacobs
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+namespace Seat\Eveapi\Observers;
+
+use Seat\Eveapi\Jobs\Alliances\Info as AllianceInfoJob;
+use Seat\Eveapi\Jobs\Corporation\Info as CorporationInfoJob;
+use Seat\Eveapi\Models\Alliances\Alliance;
+use Seat\Eveapi\Models\Character\CharacterAffiliation;
+use Seat\Eveapi\Models\Corporation\CorporationInfo;
+
+/**
+ * Class CharacterAffiliationObserver.
+ *
+ * @package Seat\Eveapi\Observers
+ */
+class CharacterAffiliationObserver
+{
+    /**
+     * @param \Seat\Eveapi\Models\Character\CharacterAffiliation $affiliation
+     */
+    public function created(CharacterAffiliation $affiliation)
+    {
+        if (! CorporationInfo::find($affiliation->corporation_id))
+            dispatch(new CorporationInfoJob($affiliation->corporation_id))->onQueue('high');
+
+        if (! Alliance::find($affiliation->alliance_id))
+            dispatch(new AllianceInfoJob($affiliation->alliance_id))->onQueue('high');
+    }
+}
