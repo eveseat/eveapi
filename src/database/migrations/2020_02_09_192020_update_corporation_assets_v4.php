7e1a1927d25aa324a9773616eec5e0eee1aa264e
Loïc LEUILLIOT <loic.leuilliot@gmail.com>
2020-02-09 19:34:17 +0100

    feat(assets): bump endpoints to v4 according to ESI changes
    https://github.com/esi/esi-issues/blob/master/changelog.md#2020-01-23

---
+++ src/database/migrations/2020_02_09_192020_update_corporation_assets_v4.php
@@ -0,0 +1,44 @@
+<?php
+
+/*
+ * This file is part of SeAT
+ *
+ * Copyright (C) 2015, 2016, 2017, 2018, 2019  Leon Jacobs
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\DB;
+
+/**
+ * Class UpdateCorporationAssetsV4.
+ */
+class UpdateCorporationAssetsV4 extends Migration
+{
+    public function up()
+    {
+        DB::statement("ALTER TABLE corporation_assets MODIFY COLUMN location_type ENUM('station','solar_system','other', 'item') NOT NULL");
+    }
+
+    public function down()
+    {
+        DB::table('corporation_assets')
+            ->where('location_type', 'item')
+            ->delete();
+
+        DB::statement("ALTER TABLE corporation_assets MODIFY COLUMN location_type ENUM('station','solar_system','other') NOT NULL");
+    }
+}
