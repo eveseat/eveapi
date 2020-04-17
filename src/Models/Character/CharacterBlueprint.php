bf4815e214c1c102bed977039ddfdbda728cdd2a
Loïc LEUILLIOT <loic.leuilliot@gmail.com>
2020-04-17 16:22:58 +0200

    refactor(relationship): add relations up to structure from blueprints

--- src/Models/Character/CharacterBlueprint.php
+++ src/Models/Character/CharacterBlueprint.php
@@ -23,7 +23,10 @@
 namespace Seat\Eveapi\Models\Character;
 
 use Illuminate\Database\Eloquent\Model;
+use Seat\Eveapi\Models\Assets\CharacterAsset;
 use Seat\Eveapi\Models\Sde\InvType;
+use Seat\Eveapi\Models\Universe\UniverseStation;
+use Seat\Eveapi\Models\Universe\UniverseStructure;
 use Seat\Eveapi\Traits\CanUpsertIgnoreReplace;
 
 /**
@@ -43,6 +46,37 @@ class CharacterBlueprint extends Model
      * @var string
      */
     protected $primaryKey = 'item_id';
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function container()
+    {
+        return $this->belongsTo(CharacterAsset::class, 'location_id', 'item_id')
+            ->withDefault();
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasOne
+     */
+    public function station()
+    {
+        return $this->hasOne(UniverseStation::class, 'station_id', 'location_id')
+            ->withDefault([
+                'name' => trans('web::seat.unknown'),
+            ]);
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasOne
+     */
+    public function structure()
+    {
+        return $this->hasOne(UniverseStructure::class, 'structure_id', 'location_id')
+            ->withDefault([
+                'name' => trans('web::seat.unknown'),
+            ]);
+    }
 
     /**
      * @return \Illuminate\Database\Eloquent\Relations\HasOne
