c37ecc25e3ced6eaa5d82e31b207db9b506a8e19
Loïc Leuilliot <loic.leuilliot@gmail.com>
2020-05-30 09:54:43 +0200

    refactor: upgrade relationships (#221)
    * fix(corporation): store corporation info only if we got a token
    
    * refactor(assets): add index on location_flag
    
    * refactor(structures): remove surrogate key
    
    * refactor(corp): improve relationship
    
    * refactor(pi): improve relationship
    
    * refactor(contract): improve relationship
    
    * refactor(char): improve relationship
    
    * refactor: use new split map models
    
    * style: apply styleCI change requests

---
+++ src/database/migrations/2020_04_19_162633_drop_corporation_id_column_from_corporation_structure_services_table.php
@@ -0,0 +1,53 @@
+<?php
+
+/*
+ * This file is part of SeAT
+ *
+ * Copyright (C) 2015 to 2020 Leon Jacobs
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+/**
+ * Class DropCorporationIdColumnFromCorporationStructureServicesTable.
+ */
+class DropCorporationIdColumnFromCorporationStructureServicesTable extends Migration
+{
+    public function up()
+    {
+        // remove any duplicated entry base on structure_id
+        DB::statement('DELETE a FROM corporation_structure_services a INNER JOIN corporation_structure_services b WHERE a.updated_at < b.updated_at AND a.structure_id = b.structure_id AND a.name = b.name');
+
+        Schema::table('corporation_structure_services', function (Blueprint $table) {
+            $table->dropPrimary(['corporation_id', 'structure_id', 'name']);
+            $table->primary(['structure_id', 'name']);
+            $table->dropColumn('corporation_id');
+        });
+    }
+
+    public function down()
+    {
+        Schema::table('corporation_structure_services', function (Blueprint $table) {
+            $table->bigInteger('corporation_id')->default(0)->first();
+            $table->dropPrimary(['structure_id', 'name']);
+            $table->primary(['corporation_id', 'structure_id', 'name'], 'corporation_structure_services_pk');
+        });
+    }
+}
