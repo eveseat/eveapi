85d07cd10be4468060f7d3a889073fdaa10e0454
warlof <e.elfaus@gmail.com>
2020-07-22 19:22:35 +0000

    Apply fixes from StyleCI

--- src/database/migrations/2020_05_31_103317_change_context_id_type_to_string_into_corporation_wallet_journals_table.php
+++ src/database/migrations/2020_05_31_103317_change_context_id_type_to_string_into_corporation_wallet_journals_table.php
@@ -21,9 +21,7 @@
  */
 
 use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Schema;
 
 /**
  * Class RenameIdColumnToReferenceIdIntoCorporationWalletJournalsTable.
