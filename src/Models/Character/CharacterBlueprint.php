f639fea6af975b45661f5ac64fffd5c5c037283f
Loïc LEUILLIOT <loic.leuilliot@gmail.com>
2021-04-02 15:22:06 +0200

    style: add missing semicolon on casts attribute

--- src/Models/Character/CharacterBlueprint.php
+++ src/Models/Character/CharacterBlueprint.php
@@ -47,13 +47,16 @@ class CharacterBlueprint extends Model
      */
     protected $primaryKey = 'item_id';
 
+    /**
+     * @var string[]
+     */
     protected $casts = [
         'location_id' => 'integer',
         'material_efficiency' => 'integer',
         'quantity' => 'integer',
         'runs' => 'integer',
         'time_efficiency' => 'integer',
-        'type_id' => 'integer'
+        'type_id' => 'integer',
     ];
 
     /**
