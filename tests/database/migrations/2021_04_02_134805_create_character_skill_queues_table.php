7ac21e7149be55475cea35f3e09f30235801404b
Loïc LEUILLIOT <loic.leuilliot@gmail.com>
2021-04-02 14:58:11 +0200

    test(jobs): add character skill queue coverage

---
+++ tests/database/migrations/2021_04_02_134805_create_character_skill_queues_table.php
@@ -0,0 +1,71 @@
+<?php
+
+/*
+ * This file is part of SeAT
+ *
+ * Copyright (C) 2015 to 2021 Leon Jacobs
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+/**
+ * Class CreateCharacterSkillQueuesTable.
+ */
+class CreateCharacterSkillQueuesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+
+        Schema::create('character_skill_queues', function (Blueprint $table) {
+
+            $table->bigIncrements('id');
+            $table->bigInteger('character_id');
+            $table->integer('skill_id');
+            $table->dateTime('finish_date')->nullable();
+            $table->dateTime('start_date')->nullable();
+            $table->integer('finished_level');
+            $table->integer('queue_position');
+            $table->integer('training_start_sp')->nullable();
+            $table->integer('level_end_sp')->nullable();
+            $table->integer('level_start_sp')->nullable();
+
+            $table->unique(['character_id', 'queue_position']);
+            $table->index(['character_id']);
+            $table->index(['queue_position']);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+
+        Schema::dropIfExists('character_skill_queues');
+    }
+}
