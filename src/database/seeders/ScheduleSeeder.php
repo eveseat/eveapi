b52f3b76d923461b7d250ad61e0bf844d0010eb6
Loïc LEUILLIOT <loic.leuilliot@gmail.com>
2022-01-01 19:07:48 +0100

    refactor: rename seeder directory to be L8 compliant

--- src/database/seeders/ScheduleSeeder.php
+++ src/database/seeders/ScheduleSeeder.php
@@ -20,7 +20,7 @@
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
-namespace Seat\Eveapi\database\seeds;
+namespace Seat\Eveapi\Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Arr;
