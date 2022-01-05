7215cefd76d25fb09301f998e806b97ee5d9ab60
Loïc Leuilliot <loic.leuilliot@gmail.com>
2022-01-05 23:39:35 +0100

    style: apply fixes from StyleCI (#325)
    Co-authored-by: StyleCI Bot <bot@styleci.io>

--- src/Mapping/Sde/InvGroupMapping.php
+++ src/Mapping/Sde/InvGroupMapping.php
@@ -15,9 +15,9 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
- *  You should have received a copy of the GNU General Public License along
- *  with this program; if not, write to the Free Software Foundation, Inc.,
- *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
 namespace Seat\Eveapi\Mapping\Sde;
@@ -36,7 +36,7 @@ use Seat\Eveapi\Models\Sde\InvGroup;
 class InvGroupMapping extends AbstractFuzzworkMapping
 {
     /**
-     * @param array $row
+     * @param  array  $row
      * @return Model|Model[]|null
      */
     public function model(array $row)
--- src/database/seeders/Sde/StaStationsSeeder.php
+++ src/database/seeders/Sde/StaStationsSeeder.php
@@ -15,9 +15,9 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
- *  You should have received a copy of the GNU General Public License along
- *  with this program; if not, write to the Free Software Foundation, Inc.,
- *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
 namespace Seat\Eveapi\Database\Seeders\Sde;
@@ -73,6 +73,7 @@ class StaStationsSeeder extends Seeder
      * Seed table with csv content.
      *
      * @return void
+     *
      * @throws \Illuminate\Contracts\Filesystem\FileNotFoundException
      */
     private function seedTable()
