1d099b837a33894b88b2acd3f28983c0cb234f38
Crypta Electrica <crypta@crypta.tech>
2023-02-16 13:40:23 +1030

    fix: remove usage of MarketPrices results

--- src/Models/Corporation/CorporationStructure.php
+++ src/Models/Corporation/CorporationStructure.php
@@ -205,8 +205,8 @@ class CorporationStructure extends Model
      */
     public function getEstimatedPriceAttribute()
     {
-        return $this->type->price->average + $this->items->sum(function ($item) {
-                return $item->type->price->average * $item->quantity;
+        return $this->type->price->adjusted_price + $this->items->sum(function ($item) {
+                return $item->type->price->adjusted_price * $item->quantity;
             });
     }
 
@@ -216,7 +216,7 @@ class CorporationStructure extends Model
     public function getFittingEstimatedPriceAttribute()
     {
         return $this->items->sum(function ($item) {
-            return $item->type->price->average * $item->quantity;
+            return $item->type->price->adjusted_price * $item->quantity;
         });
     }
 
