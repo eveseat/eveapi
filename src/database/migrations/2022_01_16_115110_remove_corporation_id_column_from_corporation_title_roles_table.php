00d035a52da9b73e95904ef7cdf8b8058b8a0ebc
recursivetree <60423027+recursivetree@users.noreply.github.com>
2023-12-26 22:09:22 +0100

    move uniqueness constraint adition (#382)

--- src/database/migrations/2022_01_16_115110_remove_corporation_id_column_from_corporation_title_roles_table.php
+++ src/database/migrations/2022_01_16_115110_remove_corporation_id_column_from_corporation_title_roles_table.php
@@ -72,8 +72,6 @@ class RemoveCorporationIdColumnFromCorporationTitleRolesTable extends Migration
             $table->dropPrimary('corporation_title_roles_primary_key');
             $table->dropColumn('corporation_id');
             $table->integer('title_id')->unsigned()->change();
-
-            $table->unique(['title_id', 'type', 'role']);
         });
 
         // move the data from the temporary column to the correct column
@@ -83,6 +81,8 @@ class RemoveCorporationIdColumnFromCorporationTitleRolesTable extends Migration
         Schema::table('corporation_title_roles', function (Blueprint $table) {
             $table->bigIncrements('id')->first();
             $table->dropColumn('title_id_temporary');
+
+            $table->unique(['title_id', 'type', 'role']);
 
             $table->foreign('title_id')
                 ->references('id')
