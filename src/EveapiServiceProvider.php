cc2db7d0923f12c194d1532829273893127991f3
recursivetree <60423027+recursivetree@users.noreply.github.com>
2025-02-11 14:48:15 +0100

    new citadel access caching strategy: Store access cache in DB (#425)
    * implement database citadel access cache
    
    * expose citadel access cache to .env
    
    * styleci fixes

--- src/EveapiServiceProvider.php
+++ src/EveapiServiceProvider.php
@@ -22,6 +22,9 @@
 
 namespace Seat\Eveapi;
 
+use Seat\Eveapi\Contracts\CitadelAccessCache;
+use Seat\Eveapi\Jobs\Universe\Structures\CacheCitadelAccessCache;
+use Seat\Eveapi\Jobs\Universe\Structures\DBCitadelAccessCache;
 use Seat\Eveapi\Models\Character\CharacterAffiliation;
 use Seat\Eveapi\Models\RefreshToken;
 use Seat\Services\AbstractSeatPlugin;
@@ -71,6 +74,12 @@ class EveapiServiceProvider extends AbstractSeatPlugin
             \Seat\Eveapi\Database\Seeders\ScheduleSeeder::class,
             // \Seat\Eveapi\Database\Seeders\Sde\SdeSeeder::class, -- Disabled until later implemented again in services
         ]);
+
+        if(env('CITADEL_ACCESS_CACHE') === 'db'){
+            $this->app->bind(CitadelAccessCache::class, DBCitadelAccessCache::class);
+        } else {
+            $this->app->bind(CitadelAccessCache::class, CacheCitadelAccessCache::class);
+        }
     }
 
     private function addCommands()
