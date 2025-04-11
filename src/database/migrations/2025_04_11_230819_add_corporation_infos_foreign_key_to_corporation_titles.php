809ec40b3fee857d3e265377e7027e615054e445
Crypta Electrica <crypta@crypta.tech>
2025-04-11 23:16:50 +0930

    feat: add corp_info foreign key to corp_titles

---
+++ src/database/migrations/2025_04_11_230819_add_corporation_infos_foreign_key_to_corporation_titles.php
@@ -0,0 +1,54 @@
+<?php
+
+/*
+ * This file is part of SeAT
+ *
+ * Copyright (C) 2015 to present Leon Jacobs
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up()
+    {
+        Schema::table('corporation_titles', function (Blueprint $table) {
+
+            // preventatively drop any orphan corporation structure row
+            DB::table('corporation_titles')
+                ->leftJoin('corporation_infos', 'corporation_titles.corporation_id', '=', 'corporation_infos.corporation_id')
+                ->whereNull('corporation_infos.corporation_id')
+                ->delete();
+
+            $table->foreign('corporation_id')
+                ->references('corporation_id')
+                ->on('corporation_infos')
+                ->onUpdate('no action')
+                ->onDelete('cascade');
+        });
+    }
+
+    public function down()
+    {
+        Schema::table('corporation_titles', function (Blueprint $table) {
+            $table->dropForeign(['corporation_id']);
+        });
+    }
+};
